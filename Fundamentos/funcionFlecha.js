61abfadc950e3039ae1246e85db04f0ebbfecf4b
Nancy Rojas <nancyrs22@hotmail.com>
2022-04-25 14:03:53 -0500

    Convertir a flecha

---
+++ Fundamentos/funcionFlecha.js
@@ -0,0 +1,46 @@
+
+/*class Deck {
+    constructor() {
+        const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
+        const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+        const deck = [];
+        for (const suit of suits) {
+            for (const face of faces) {
+            deck.push(this.createCard(suit, face));
+            }
+        }
+        this.deck = deck;
+    }
+}
+*/
+
+/*
+class Deck {
+      constructor() {
+          const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
+          const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+          const deck = [];
+          suits.forEach(function(suit) {
+            faces.forEach(function(face) {
+              deck.push(this.createCard(suit, face));
+            });
+          });
+          this.deck = deck;
+        }
+    }
+*/
+
+class Deck {
+      constructor() {
+          const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
+          const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+          const deck = [];
+              suits.forEach(suit => {
+            faces.forEach(face => {
+            deck.push(this.createCard(suit, face));
+            });
+        });
+        }
+    
+    }
+
