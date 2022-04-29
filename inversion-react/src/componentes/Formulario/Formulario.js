784eb108f63c573f602104b95897351e713e4263
Nancy Rojas <nancyrs22@hotmail.com>
2022-04-29 14:27:29 -0500

    Mas formularios

--- inversion-react/src/componentes/Formulario/Formulario.js
+++ inversion-react/src/componentes/Formulario/Formulario.js
@@ -40,6 +40,43 @@ const UserForm = () =>
     //     }
     // }
 
+    const mensajeNombre = () =>
+    {
+        if(hizoSubmit)
+        {
+            if(firstName.length <= 2 || lastName.length <= 2)
+            {
+                return "el nombre y el apellido deben tener mas de 2 caracteres.";
+            }
+        }
+    }
+
+    const mensajeEmail = () =>
+    {
+        if(hizoSubmit)
+        {
+            if(email.length <=5)
+            {
+                return "El campo email debe tener mas de 5 caracteres.";
+            }
+            if(password !== confirm)
+            {
+                return "la contraseña y la confirmacion no son iguales";
+            }
+        }
+    }
+
+    const mensajePassword = () =>
+    {
+        if(hizoSubmit)
+        {
+            if(password !== confirm)
+            {
+                return "la contraseña y la confirmacion no son iguales";
+            }
+        }
+    }
+
     return(
         <div>
             <form onSubmit={createUser}>
@@ -47,26 +84,31 @@ const UserForm = () =>
                 <label>Fist Name: </label>
                 <input type="text" onChange={(e)=>setFirstName(e.target.value)} value={firstName}/>
             </div>
+            {mensajeNombre()}
             <div>
                 <label>Last Name: </label>
                 <input type="text" onChange={(e)=>setLastName(e.target.value)} value={lastName}/>
             </div>
+            {mensajeNombre()}
             <div>
                 <label>Email: </label>
                 <input type="text" onChange={(e)=>setEmail(e.target.value)} value={email}/>
             </div>
+            {mensajeEmail()}
             <div>
                 <label>Password: </label>
                 <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
             </div>
+            {mensajePassword()}
             <div>
                 <label>Confirm password: </label>
                 <input type="password" onChange={(e)=>setConfirm(e.target.value)} value={confirm}/>
             </div>
+            {mensajePassword()}
             <input type="submit" value="Create User"></input>
-            
             </form>
             {/* {dataForm()} */}
+            
             <p>Your Form Data</p>
             <p>First Name: {firstName}</p>
             <p>Last Name: {lastName}</p>
